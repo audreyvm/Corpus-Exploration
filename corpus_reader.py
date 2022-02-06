97ad48133615b8e7b0b2a7ad00eeb0363b3fdd38
audreyvm <96874368+audreyvm@users.noreply.github.com>
2022-02-06 14:11:25 +0100

    creates database

--- corpus_reader.py
+++ corpus_reader.py
@@ -8,6 +8,7 @@ bnc_reader = bnc(root="BNC/Texts", fileids=r'[A-K]/\w*/\w*\.xml')
 adjs = pd.read_csv('Adjective_list.csv')
 adj_list =adjs['Adjective'].tolist()
 
+
 #makes it iterable with element tree
 def get_root(fileid):
     filename=os.path.join('','BNC','Texts',fileid)
@@ -18,8 +19,6 @@ def get_root(fileid):
 #turns the sentence into a list of first ID and then tuples
 def get_sentence(tree_root):
     sentence = []
-    sent_id = s.get('n')
-    sentence.append(sent_id)
     for w in s.iter('w'):
             pos = w.get('c5')
             lemma = w.get('hw')
@@ -28,46 +27,27 @@ def get_sentence(tree_root):
             sentence.append(word_tuple)
     return sentence
 
- 
-#identifies use of adjective
-def ident_type(sentence):
-    pos = [i[0] for i in sentence [1:]]
-    words = [i[2] for i in sentence [1:]]
+def make_strings(sentence):
+    pos = [i[0] for i in sentence]
+    lemmas = [i[1] for i in sentence]
+    words = [i[2] for i in sentence]
+    words = [word.rstrip() for word in words]
     pos_string =" ".join(pos)
-    words_string ="".join(words)
-    pp_pattern = r'(PRP|PRF)\s(DT0|AT0\s)*(AVP\s)*(AJ0\s)+(NN1|NN2)' 
-    mod_pattern = r".*(AJ0)+\s(NN1|NN2)"
+    words_string =" ".join(words)
+    lemmas_string =" ".join(lemmas)
+    return pos_string, lemmas_string, words_string
 
-    x = re.search(pp_pattern, pos_string)
-    y = re.search(mod_pattern, pos_string)
-    
-    if x:
-        print('\nIts a PP\n',words_string)
-    elif y:
-        print('\nIts a modifier\n',words_string)
-    else: print('\nMust be a predicate\n', words_string)
-    
-    return pos_string, words_string
-
-    
 #gets target word
 def get_target(sentence):
-    pos = [i[0] for i in sentence [1:]]
-    lemmas = [i[1] for i in sentence[1:]]
-    words = [i[2] for i in sentence [1:]]
+    pos = [i[0] for i in sentence]
+    lemmas = [i[1] for i in sentence]
+    words = [i[2] for i in sentence]
     for p, l, w in zip(pos,lemmas, words):
         if l in adj_list and p == 'AJ0':
-            print(p,w)
-            
-# def get_head_noun(sentence):
-#     pos = [i[0] for i in sentence [1:]]
-#     words = [i[2] for i in sentence [1:]]    
-#     ind = get_index(sentence)
-#     for i in pos:
-#         if index(i)>ind and pos == 'NN1' or pos == 'NN2':
-            
+            return w                 
     
-tuple_list = []    
+tuple_list = []
+id_lst =[]    
 count = 0        
 for fileid in bnc_reader.fileids():
     count += 1
@@ -78,19 +58,45 @@ for fileid in bnc_reader.fileids():
                 pos = w.get('c5')
                 lemma = w.get('hw')
                 if pos == 'AJ0' and lemma in adj_list:
+                    sent_id = s.get('n')
+                    file_id = fileid
+                    new_id = sent_id + ' ' + file_id[5:8]
+                    id_lst.append(new_id)
                     tuples = get_sentence(file_root)
-                    tuple_list.append(tuples)
-                    
+                    tuple_list.append(tuples)                    
     else: break
-count = 0
+
+pos_lst = []
+lemmas_lst = []
+words_lst = []
+target_lst = []
+
 for sent in tuple_list:
-    ident_type(sent)
-    pos = [i[0] for i in sent[1:]]
-    lemmas = [i[1] for i in sent[1:]]
-    words = [i[2] for i in sent[1:]]
-    for p, l, w in zip(pos,lemmas, words):
-        if l in adj_list and p == 'AJ0':
-            print(p,w)
+    pos, lemmas, words = make_strings(sent)
+    target = get_target(sent)
+    pos_lst.append(pos)
+    lemmas_lst.append(lemmas)
+    words_lst.append(words)
+    target_lst.append(target)
+    
+    
+lst_tuples = list(zip(id_lst, target_lst, pos_lst, lemmas_lst, words_lst))
+data = pd.DataFrame(lst_tuples, columns=['ID', 'Target Adj', 'POS','Lemmas', 'Words'])
+data = data.set_index('ID')
+print('Dataframe created')
+    
+#identifies use of adjective
+def ident_type(sentence):
+    pos, lemmas, words = make_strings(sentence)
+    pp_pattern = r'(PRP|PRF)\s(DT0|AT0\s)*(AVP\s)*(AJ0\s)+(NN1|NN2)' 
+    mod_pattern = r".*(AJ0)+\s(NN1|NN2)"
 
-
+    x = re.search(pp_pattern, pos)
+    y = re.search(mod_pattern, pos)
+    
+    if x:
+        print('\nIts a PP\n',words)
+    elif y:
+        print('\nIts a modifier\n',words)
+    else: print('\nMust be a predicate\n', words)
 
