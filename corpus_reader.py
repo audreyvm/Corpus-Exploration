57dddc77896c7ce1b5142575410e35fe74683f80
audreyvm <96874368+audreyvm@users.noreply.github.com>
2022-02-06 17:31:07 +0100

    rearranged
    accounts for multiple target words in a single sentence

--- corpus_reader.py
+++ corpus_reader.py
@@ -67,8 +67,7 @@ def pos_string(string):
         x = str(x)
         pos_lst.append(x)
     return pos_lst
-                
-    
+                    
 tuples_lst = []
 lemmas_lst = []
 words_lst = []
@@ -80,81 +79,49 @@ type_lst=[]
 count = 0        
 for fileid in bnc_reader.fileids():
     count += 1
-    if count <=1:
+    if count <=4:
         file_root = get_root(fileid)
         for s in file_root.iter('s'):
             tuples = get_sentence(file_root)
             lemmas, words = make_strings(tuples)
             sent_id = s.get('n')
             if has_target(words) == True:
-                tuples_lst.append(tuples)
-                lemmas_lst.append(lemmas)
-                words_lst.append(words)
-                target = get_target(words)
-                target_lst.append(target)
-                pos = pos_string(words)
-                pos_lst.append(pos)
-                file_id = fileid
-                new_id = sent_id + ' ' + file_id[5:8]
-                id_lst.append(new_id)
                 txt = nlp(words)
                 for token in txt:
                     if token.text in adj_list and token.pos_ == 'ADJ':
                         headnoun_lst.append(token.head.text)
+                        tuples_lst.append(tuples)
+                        lemmas_lst.append(lemmas)
+                        words_lst.append(words)
+                        target = get_target(words)
+                        target_lst.append(target)
+                        pos = pos_string(words)
+                        pos_lst.append(pos)
+                        file_id = fileid
+                        new_id = sent_id + ' ' + file_id[5:8]
+                        id_lst.append(new_id)
                     else: continue
                 for token in txt:
                     x = token.head
                     if token.text in adj_list and token.pos_ == 'ADJ':
                             if x.head.dep_ == 'prep':
                                 type_lst.append('PP')
+                            elif x.pos_ == 'VERB':
+                                type_lst.append('Predicate')
+                            elif x.pos_ == 'NOUN':
+                                type_lst.append('Non-PP Mod')
                             else: type_lst.append('other')
     else: break
 print('creating database')
 lst_tuples = list(zip(id_lst, target_lst,headnoun_lst, pos_lst, lemmas_lst, words_lst, type_lst))
-data = pd.DataFrame(lst_tuples, columns=['ID', 'Target Adj','Head Noun', 'POS','Lemmas', 'Words', 'Type'])
+data = pd.DataFrame(lst_tuples, columns=['ID', 'Target Adj','Head word', 'POS','Lemmas', 'Words', 'Type'])
 data = data.set_index('ID')
 print('Dataframe created')
 
-
-# for sent in tuple_list:
-#     lemmas, words = make_strings(sent)
-#     target = get_target(sent)
-#     pos_lst.append(pos)
-#     lemmas_lst.append(lemmas)
-#     words_lst.append(words)
-#     target_lst.append(target)
-#     txt = nlp(words)
-
-
-
-
-# sentence = words_lst[13]
+sentence = data.loc[["57 A02"],['Words']]
+print(sentence)
+                    
 # txt = nlp(sentence)
 # for token in txt:
 #     if token.text in adj_list and token.pos_ == 'ADJ':
 #         print(token, token.pos_ ,token.dep_,token.head)
-
-
-
-# # for string in words_lst:
-# #     count += 1
-# #     txt = nlp(string)
-# #     if count <1:
-# #         print(txt)
-# #         for token in txt:
-# #             print(token.text, token.head.text)
-    
-# # #identifies use of adjective
-# # def ident_type(sentence):
-# #     pos, lemmas, words = make_strings(sentence)
-# #     pp_pattern = r'(PRP|PRF)\s(DT0|AT0\s)*(AVP\s)*(AJ0\s)+(NN1|NN2)' 
-# #     mod_pattern = r".*(AJ0)+\s(NN1|NN2)"
-
-# #     x = re.search(pp_pattern, pos)
-# #     y = re.search(mod_pattern, pos)
-    
-# #     if x:
-# #         print('\nIts a PP\n',words)
-# #     elif y:
-# #         print('\nIts a modifier\n',words)
-# #     else: print('\nMust be a predicate\n', words)
