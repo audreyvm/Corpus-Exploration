6885d7221f09ff174d43655705b99ffc53058317
audreyvm <96874368+audreyvm@users.noreply.github.com>
2022-03-21 15:49:29 +0100

    with dataframe

--- adj_finder.py
+++ adj_finder.py
@@ -6,12 +6,11 @@ Created on Sat Mar 12 09:02:26 2022
 """
 
 from nltk.corpus.reader.bnc import BNCCorpusReader as bnc
-import en_core_web_sm
 import re
 import collections
-
+import pandas as pd
 import Preprocessor as pp
-nlp = en_core_web_sm.load()
+
 
 bnc_reader = bnc(root="BNC/Texts", fileids=r'[A-K]/\w*/\w*\.xml')
 
@@ -33,26 +32,31 @@ rel_counts = collections.Counter(rel_adj_list)
 common_rel_adjs = [adj for adj, c in rel_counts.items() if c>=30]
 rel_100=[adj for adj, c in rel_counts.most_common(100)]
 
+
 qual_counts = collections.Counter(qual_adj_list)
 qual_100=[adj for adj, c in qual_counts.most_common(100)]
 
-al_list = []
-ary_list = []
-an_list = []
-ical_list = []
-ic_list = []
+adjs = pd.DataFrame(rel_100)
+adjs['Qualitative'] = qual_100
+adjs.to_csv('adj_lists.csv')
 
-for adj in common_rel_adjs:
-    if re.search('.*al$', adj):
-        al_list.append(adj)
-    elif re.search('.*ary$', adj):
-        ary_list.append(adj)
-    elif re.search('.*an$', adj):
-        an_list.append(adj)
-    elif re.search('.*ic$', adj):
-        ic_list.append(adj)        
+# al_list = []
+# ary_list = []
+# an_list = []
+# ical_list = []
+# ic_list = []
 
-with open('adjectives.txt', 'w') as f:
-    for item in ic_list:
-        f.write("%s\n" % item)
+# for adj in common_rel_adjs:
+#     if re.search('.*al$', adj):
+#         al_list.append(adj)
+#     elif re.search('.*ary$', adj):
+#         ary_list.append(adj)
+#     elif re.search('.*an$', adj):
+#         an_list.append(adj)
+#     elif re.search('.*ic$', adj):
+#         ic_list.append(adj)        
+
+# with open('adjectives.txt', 'w') as f:
+#     for item in ic_list:
+#         f.write("%s\n" % item)
         
